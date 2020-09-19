6f21289a068e50ad9bb756fbc7b22b88ed50bce5
Alexander Eichhorn <a.eichhorn@losjet.com>
2020-09-19 17:15:58 +0200

    updated checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "CatalystSparkle",
             url: "https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.1.1/SparkleBridgeClient-0.1.1.xcframework.zip",
-            checksum: "b532177a14bc924684f81b777d4fd5489283458e8e50a25f9b5d57d241d9f2c5"
+            checksum: "a130bb117e86f3420866b8bca237232ef9730fb7010f0e54ba5eba5ee5b28b64"
         )
     ]
 )
