4928ed4ba21b63aa2fe45df96e63fbce62a51858
Alexander Eichhorn <a.eichhorn@losjet.com>
2020-09-19 16:21:02 +0200

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "CatalystSparkle",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "CatalystSparkle", targets: ["CatalystSparkle"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CatalystSparkle",
+            url: "https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.0.1/SparkleBridgeClient-0.1.0.xcframework.zip",
+            checksum: "b7a6e25575fd4db2869fb63f62fc5b743538d42e9f6e179c294696f116643618"
+        )
+    ]
+)
