693826a232a1dd21de92a0f63ed5b24268b533a4
Alexander Eichhorn <a.eichhorn@losjet.com>
2020-09-20 18:27:54 +0200

    reduced xcframework size for unused platforms

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SparkleBridgeClient",
-            url: "https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.1.2/SparkleBridgeClient-0.1.2.xcframework.zip",
-            checksum: "3165278ee8433dcf101586de36bebff34b5f3984ee904bc67052cfa89198abb3"
+            url: "https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.1.3/SparkleBridgeClient-0.1.3.xcframework.zip",
+            checksum: "ad0e4ab865af0e5615219842f431d5147a11af846607ddb28bd7dd225dd54eb7"
         )
     ]
 )
