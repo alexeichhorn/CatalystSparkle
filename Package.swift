d21bd1e8f23d84f9cf8aadee6516f515e3b80404
Alexander Eichhorn <a.eichhorn@losjet.com>
2021-05-16 19:14:38 +0200

    bug fix when pressing install

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "SparkleBridgeClient",
             url: "https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.1.7/SparkleBridgeClient-0.1.7.xcframework.zip",
-            checksum: "8180278d8ff6bc0fed36663c5b6fcaba8007c232e1da7615618818a33e2da10b"
+            checksum: "8b468708fa1cec16f809b97e68102af81e3b934656401084363365d1d2167793"
         )
     ]
 )
