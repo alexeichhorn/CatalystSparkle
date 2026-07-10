307272cbbdfe96a4ce1525d22eff0789771d517b
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-07-10 13:59:25 +0200

    Release 0.4.3

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
         .binaryTarget(
             name: "SparkleBridgeClient",
             url: 
-"https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.4.2/SparkleBridgeClient-0.4.2.xcframework.zip",
+"https://github.com/alexeichhorn/CatalystSparkle/releases/download/0.4.3/SparkleBridgeClient-0.4.3.xcframework.zip",
             checksum:
-"80a0271fae496d6c2dede8a2e9df16e3077211e77bbf4832a8c79f9d170bf2a2"
+"948094c4c7fcd801aca0d2770ee8c2360d95005f5d75a3c32c65fbbca2d884c1"
         )
     ]
 )
